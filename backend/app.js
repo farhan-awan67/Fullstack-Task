cecd34193a646f14b8c57950381736b16496b8b2
farhan-awan67 <farhanawxn@gmail.com>
2025-04-23 10:11:42 +0500

    added urls

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,17 @@ import cors from "cors";
 import connectDB from "./config/db.js";
 import productRoute from "./routes/product.route.js";
 
+//cors options
+const corsOptions = {
+  origin: "https://fullstack-task-eight.vercel.app",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
 //middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors(corsOptions));
 
 //PORT
 const PORT = process.env.PORT || 3000;
